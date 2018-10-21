cfab5693387def7755b7bd848ca52cc530336b0f
dleidi <dleidi@suse.com>
2018-10-21 15:10:21 +0200

    Improve golden-ratio output

--- golden-ratio.py
+++ golden-ratio.py
@@ -3,6 +3,9 @@
 import sys
 
 def main():
+   fi = 1.6180339887
+   sys.stdout.write('[Golden-ratio irrational proportion value is ' + str(fi) + '..]\n')
+
    if len(sys.argv) < 4:
       sys.stdout.writelines('Argument missing: command format should look like the following\n' +
           '  "python <program-name> limit[number] approxDecimal[number] step[number]"\n')
@@ -23,8 +26,12 @@ def main():
             pairs.append({'a' : a , 'b' : b});
          a += step
 
-   sys.stdout.write(str(pairs))
-   sys.stdout.write('\n')
+   if len(pairs) == 0:
+      sys.stdout.write('No pair found\n')
+   else:
+      for pair in pairs:
+         sys.stdout.write('a: ' + str(pair.get('a')) + ' , b: ' + str(pair.get('b')))
+         sys .stdout.write('\n')
 
 if __name__ == "__main__":
    main()
