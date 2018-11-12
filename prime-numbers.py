6fba24c7170c1890ad05027da0f63029a12c120c
Dario Leidi <dario.leidi@gmail.com>
2018-11-12 23:21:06 +0100

    Add some skip rules to compute only on possible values

--- prime-numbers.py
+++ prime-numbers.py
@@ -5,13 +5,36 @@ import sys
 def isDivisorOf(d, n):
    return n%d == 0
 
+def sumSingleDigits(n):
+   sum = 0
+   for i in range(0, len(str(n))):
+      sum += int(str(n)[i])
+   return sum
+
+def minimizeBySum(n):
+   while len(str(n)) > 1:
+      n = sumSingleDigits(n)
+   return n
+
 def isPrime(n):
    if n == 1:
       return True
 
+   # let's just do the skipping only after the first tens of numbers
+   if n > 10:
+      # skip all numbers divisible by 3 because the sum of single digits is a multiple of 3
+      if minimizeBySum(n) % 3 == 0:
+         return False
+      # skip all odd numbers divisible by 5
+      if str(n).endswith('5') and n > 5:
+         return False
+
+   # divisor range from 3 with a step of 2 so we skip all even divisors: if divisor was even,
+   # we would have an even number, and even means it can be divided always also by 2,
+   # so it cannot be a prime number at all.
    # highest divisor 'i' cannot be no greater than the half of the number 'n',
    # as 'n' would then be a multiple by 2 of 'i'
-   for i in range(2, (n/2)+1) :
+   for i in range(3, (n/2)+1, 2) :
       if isDivisorOf(i, n):
          return False
 
@@ -26,8 +49,18 @@ def main():
    primes = []
 
    limit = int(sys.argv[1])
+   min_limit = 1
 
-   for n in range(1, limit):
+   if len(sys.argv) == 3:
+       min_limit = int(sys.argv[1])
+       limit = int(sys.argv[2])
+
+   # start from the first odd value
+   if min_limit % 2 == 0:
+       min_limit = min_limit + 1
+
+   # step of 2 starting from the first odd value to skip all even values
+   for n in range(min_limit, limit, 2):
       if isPrime(n):
         primes.append(n)
 
