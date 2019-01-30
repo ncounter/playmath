2f40a268b18d49ff58933492bb2ecf393dc7e775
Dario Leidi <dario.leidi@gmail.com>
2019-01-31 00:23:14 +0100

    Fix first prime number which is '2'. Improve comments and output as well

--- prime-numbers.py
+++ prime-numbers.py
@@ -16,11 +16,12 @@ def minimizeBySum(n):
       n = sumSingleDigits(n)
    return n
 
+# isPrime receives only odd numbers, there is no need to check for even numbers or for (n%2==0)
 def isPrime(n):
    if n == 1:
       return True
 
-   # let's just do the skipping only after the first tens of numbers
+   # let's just do the skipping only after the first ten numbers
    if n > 10:
       # skip all numbers divisible by 3 because the sum of single digits is a multiple of 3
       if minimizeBySum(n) % 3 == 0:
@@ -30,10 +31,10 @@ def isPrime(n):
          return False
 
    # divisor range from 3 with a step of 2 so we skip all even divisors: if divisor was even,
-   # we would have an even number, and even means it can be divided always also by 2,
-   # so it cannot be a prime number at all.
+   # we would have an even number, and even means it can be divided always also by the number 2,
+   # so it couldn't be a prime number, definitely.
    # highest divisor 'i' cannot be no greater than the half of the number 'n',
-   # as 'n' would then be a multiple by 2 of 'i'
+   # as 'n' would then be a multiple by 2 of 'i': setting the max range value up to '(n/2)-1' then
    for i in range(3, (n/2)+1, 2) :
       if isDivisorOf(i, n):
          return False
@@ -48,9 +49,11 @@ def main():
 
    primes = []
 
+   # by default 1 argument as a minimal value and it is the upper limit
    limit = int(sys.argv[1])
-   min_limit = 1
+   min_limit = 3
 
+   # if there are more arguments, the order matters: min_limit first, then upper limit
    if len(sys.argv) == 3:
        min_limit = int(sys.argv[1])
        limit = int(sys.argv[2])
@@ -59,11 +62,19 @@ def main():
    if min_limit % 2 == 0:
        min_limit = min_limit + 1
 
-   # step of 2 starting from the first odd value to skip all even values
-   for n in range(min_limit, limit, 2):
+   # start from 3 as a minimum if set differently
+   if min_limit < 3:
+       min_limit = 3
+
+   primes.append(2) # because we know the first prime number (and the only even one) is number 2
+
+   # move ahead of a "step of 2 numbers", starting from the first odd value, to skip all even values
+   for n in range(min_limit, limit+1, 2):
       if isPrime(n):
         primes.append(n)
 
+   sys.stdout.write('## ' + str(len(primes)) + ' prime numbers found ##')
+   sys.stdout.write('\n')
    sys.stdout.write(str(primes))
    sys.stdout.write('\n')
 
