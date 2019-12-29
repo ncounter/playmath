3cc08d1008ff67b9a52010d1ed91db68a5e5b413
dleidi <dleidi@suse.com>
2019-12-29 23:40:17 +0100

    Improve console output and help messages

--- golden-ratio.py
+++ golden-ratio.py
@@ -15,17 +15,22 @@ def check_parameters(arguments):
          return True
       else:
          sys.stdout.write('Argument missing: command format should look like the following\n' +
-         '  "python golden-ratio.py pair limit<number> approxDecimal<number> step<number>"\n')
+         '  "python golden-ratio.py pairs limit<number> approxDecimal<number> step<number>"\n')
          return False
    elif f_name == 'split':
       if len(arguments) == 4:
          return True
       else:
          sys.stdout.write('Argument missing: command format should look like the following\n' +
-         '  "python golden-ratio.py sum<number> approxDecimal<number>"\n')
+         '  "python golden-ratio.py split sum<number> approxDecimal<number>"\n')
          return False
    else:
       sys.stdout.write('Unknown function "' + f_name + '"\n')
+      sys.stdout.write('Available functions are\n'+
+                        '- pairs: find pairs of number below the given limit<number>, the approxDecimal<number> approximation '+
+                          'and the step<number> pace to move during the research\n' +
+                        '- split: bisect the given sum<number> into two numbers where the results are in a "golden ratio" ' +
+                          'relationship, respecting the given approxDecimal<number> approximation\n')
       return False
 
 def split(arguments):
@@ -33,7 +38,11 @@ def split(arguments):
    approxDecimal = int(arguments[3])
    a = round(sum / FI, approxDecimal)
    b = sum - a
-   sys.stdout.write('a: ' + str(a) + ' , b: ' + str(b) + '\n')
+
+   sys.stdout.write('a = ' + str(a) + ' , b = ' + str(b) +
+           ' , a/b = ' + str(a / b) +
+           ' ==> round(a/b, ' + str(approxDecimal) + ') = ' + str(round(a / b, approxDecimal)))
+   sys.stdout.write('\n')
 
 def pairs(arguments):
    pairs = []
@@ -55,11 +64,15 @@ def pairs(arguments):
       sys.stdout.write('No pair found\n')
    else:
       for pair in pairs:
-         sys.stdout.write('a: ' + str(pair.get('a')) + ' , b: ' + str(pair.get('b')))
-         sys .stdout.write('\n')
+         sys.stdout.write('a = ' + str(pair.get('a')) + ' , b = ' + str(pair.get('b')) +
+           ' , a/b = ' + str(pair.get('a') / pair.get('b')) +
+           ' ==> round(a/b, ' + str(approxDecimal) + ') = ' + str(round(pair.get('a') / pair.get('b'), approxDecimal)))
+         sys.stdout.write('\n')
 
 def main():
+   sys.stdout.write('*************************************************************\n')
    sys.stdout.write('[Golden-ratio irrational proportion value is ' + str(FI) + '..]\n')
+   sys.stdout.write('*************************************************************\n')
 
    if not check_parameters(sys.argv):
       return
